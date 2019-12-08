3f544000301ef913e58c5d4fccfe511607fe2289
zbilalozgen <zahidbilalozgen@gmail.com>
2019-12-08 03:16:46 +0300

    Updates

---
+++ precache-manifest.f931b4baaae834c986058b366c065ba3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e452b4fdd3e58ac0c7f2c2f8136dbd48",
+    "url": "/batman-shows-app/index.html"
+  },
+  {
+    "revision": "133acd0c58f2cb2cf02e",
+    "url": "/batman-shows-app/static/css/2.a7daffc7.chunk.css"
+  },
+  {
+    "revision": "1775f55dc68a93d45c43",
+    "url": "/batman-shows-app/static/css/main.120ed4b2.chunk.css"
+  },
+  {
+    "revision": "133acd0c58f2cb2cf02e",
+    "url": "/batman-shows-app/static/js/2.81994e64.chunk.js"
+  },
+  {
+    "revision": "1eba9df205d06c4b05b7096999ba3bc3",
+    "url": "/batman-shows-app/static/js/2.81994e64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1775f55dc68a93d45c43",
+    "url": "/batman-shows-app/static/js/main.72899a87.chunk.js"
+  },
+  {
+    "revision": "0eeae923b33f948ec9fd",
+    "url": "/batman-shows-app/static/js/runtime-main.103daddd.js"
+  },
+  {
+    "revision": "ae7fc5d09ee48b4c85801d66640350fd",
+    "url": "/batman-shows-app/static/media/batarang.ae7fc5d0.svg"
+  }
+]);
